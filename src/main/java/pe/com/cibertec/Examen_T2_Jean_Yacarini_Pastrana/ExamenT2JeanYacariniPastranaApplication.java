89281cfda738bad6c4f95d5ffc66fc98042ae926
Yankii01 <jean_antoni@hotmail.com>
2024-10-13 19:10:58 -0500

    Estructura inicial del examen T2

---
+++ src/main/java/pe/com/cibertec/Examen_T2_Jean_Yacarini_Pastrana/ExamenT2JeanYacariniPastranaApplication.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec.Examen_T2_Jean_Yacarini_Pastrana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenT2JeanYacariniPastranaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenT2JeanYacariniPastranaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/com/cibertec/Examen_T2_Jean_Yacarini_Pastrana/ExamenT2JeanYacariniPastranaApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec.Examen_T2_Jean_Yacarini_Pastrana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenT2JeanYacariniPastranaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
